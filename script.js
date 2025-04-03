eb5169a6b8e59a2f851e53d521ac603426632f92
leo-gasparoto <leonardo.zanforlin@aluno.senai.br>
2025-04-03 07:49:22 -0300

    Subindo Exercicio 2

--- script.js
+++ script.js
@@ -1,44 +1,53 @@
 let titulo = document.getElementById("titulo");
-console.log(titulo);
+titulo.textContent = "Meus Esportes Favoritos"
+titulo.style.color = "blue"
 
-//CLASSE
-let descricao1 = document.getElementById("descricao1")
-console.log(descricao1[0]);
+// //CLASSE
+// let descricao1 = document.getElementById("descricao1")
+// console.log(descricao1[0]);
 
-let descricao2 = document.getElementById("descricao2")
-console.log(descricao2[1]);
+// let descricao2 = document.getElementById("descricao2")
+// console.log(descricao2[1]);
 
-let descricao3 = document.getElementById("descricao3")
-console.log(descricao3[2]);
+// let descricao3 = document.getElementById("descricao3")
+// console.log(descricao3[2]);
 
-let descricao4 = document.getElementById("descricao4")
-console.log(descricao4[3]);
+// let descricao4 = document.getElementById("descricao4")
+// console.log(descricao4[3]);
 
 
-//TAG
-let liEsporte1 = Document.getElementByTagName("li");
-console.log(liEsportes[0]);
+// //TAG
+// let liEsporte1 = Document.getElementByTagName("li");
+// console.log(liEsportes[0]);
 
-let liEsporte2 = Document.getElementByTagName("li");
-console.log(liEsportes2[1]);
+// let liEsporte2 = Document.getElementByTagName("li");
+// console.log(liEsportes2[1]);
 
-let liEsporte3 = Document.getElementByTagName("li");
-console.log(liEsportes3[2]);
+// let liEsporte3 = Document.getElementByTagName("li");
+// console.log(liEsportes3[2]);
 
-let liEsporte4 = Document.getElementByTagName("li");
-console.log(liEsportes4[3]);
+// let liEsporte4 = Document.getElementByTagName("li");
+// console.log(liEsportes4[3]);
 
 //CSS
 
-let tituloQS = document.querySelector("#titulo");
-let liEsporteQS = document.querySelector(".descricao");
+const tituloQS = document.querySelector("#titulo");
 console.log("Exibindo com QS" + tituloQS.textContent)
-console.log("Exibindo com QS" + liEsporte1[0].textContent)
-console.log("Exibindo com QS" + liEsportes2[1].textContent)
-console.log("Exibindo com QS" + liEsportes3[2].textContent)
-console.log("Exibindo com QS" + liEsportes4[3].textContent)
 
 
+let liEsporteQS = document.querySelectorAll(".descricao");
+console.log("Exibindo com QS" + liEsporteQS[0].textContent)
 
 
+const lista = document.querySelector("ul"); 
+const novoEsporte1 = document.createElement("li");
+novoEsporte1.textContent = "Beisebol";
+novoEsporte1.classList.add("esporte");
+
+const novoEsporte2 = document.createElement("li");
+novoEsporte2.textContent = "Natação";
+novoEsporte2.classList.add("esporte");
+
+lista.appendChild(novoEsporte1);
+lista.appendChild(novoEsporte2);
 
